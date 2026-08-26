dc36caf23ad505b7c5008eb2c49c52d49f244fdb
daarain <daarainkazi@gmail.com>
2026-08-26 12:35:20 +0530

    commit

---
+++ Frontend/page.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Page() {
+  return (
+    <div>
+      <h1>Welcome to MarianaTech</h1>
+      <p>This is the main page of our React application.</p>
+    </div>
+  );
+}   
